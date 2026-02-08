b6b2bc262b9ea7cccad0fe509409387e0c80a0d8
Farhan41229 <farhankhan@iut-dhaka.edu>
2026-02-08 18:54:30 +0600

    Shadcn Installed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { RouterProvider } from 'react-router';
 import router from './routes/Router';
-
+import { Button } from '@/components/ui/button';
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
